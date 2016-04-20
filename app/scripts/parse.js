2b9607381732ab8029781350974cf32ab69f31f4
SimoneConia <simoneconia@gmail.com>
2016-04-20 18:35:07 +0100

    Add Parse SDK initialization.

--- app/scripts/parse.js
+++ app/scripts/parse.js
@@ -1,6 +1,11 @@
-(function(document) {
+( function( document ) {
   'use strict';
 
-  
+  try {
+    Parse.initialize( "JdgT5RtwJaaoDRNe9bx5LF3TyVj20RGXMKMv6jQ8", "MttzOIoQIxkHFd6IuCrsk8AG4FeUy0jTP741gtIg" );
+    // alert( "Success!" );
+  } catch( error ) {
+    alert( "Failed to initialize webapp connection to the database. Error: " + error.message );
+  }
 
-})(document);
+})( document );
